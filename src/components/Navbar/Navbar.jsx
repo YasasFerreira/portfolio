a1cf4bee7b1e1aabfe3cfad2b0647ded1d93631a
Yasas Ferreira <yasasferreira05@gmail.com>
2025-12-05 23:00:40 +0530

    updated navbar

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -11,8 +11,8 @@ const Navbar = () => {
 
   <div className={`nav-links ${menuOpen ? "active" : ""}`}>
     <a href="#home" onClick={() => setMenuOpen(false)}>Home</a>
-    <a href="#about" onClick={() => setMenuOpen(false)}>About</a>
     <a href="#skills" onClick={() => setMenuOpen(false)}>Skills</a>
+    <a href="#education" onClick={() => setMenuOpen(false)}>Education</a>
     <a href="#projects" onClick={() => setMenuOpen(false)}>Projects</a>
     <a href="#contact" onClick={() => setMenuOpen(false)}>Contact</a>
   </div>
